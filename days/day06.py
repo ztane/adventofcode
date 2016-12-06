17803bc6f1a5d2a15027db305a0c7a6221766565
Antti Haapala <antti.haapala@proxion.fi>
2016-12-06 10:20:36 +0200

    solution for day 6.

--- days/day06.py
+++ days/day06.py
@@ -2,9 +2,23 @@ from helpers import *
 
 d = get_aoc_data(day=6)
 
+
 def part1():
-    ...
+    counters = [Counter() for i in range(len('bgpmxqws'))]
+    for i in d.lines():
+        for j, c in enumerate(i):
+            counters[j].update(c)
+
+    message = ''.join([c.most_common(1)[0][0] for c in counters])
+    print(message)
+
 
 def part2():
-    ...
+    counters = [Counter() for i in range(len('bgpmxqws'))]
+    for i in d.lines():
+        for j, c in enumerate(i):
+            counters[j].update(c)
+
+    message = ''.join([c.most_common()[-1][0] for c in counters])
+    print(message)
 
