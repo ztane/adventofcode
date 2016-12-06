bde2189747ce0d98e289443d6f3fc47de0fdf8e5
Antti Haapala <antti.haapala@proxion.fi>
2016-12-06 10:29:01 +0200

    made it prettier with zip

--- days/day06.py
+++ days/day06.py
@@ -4,20 +4,20 @@ d = get_aoc_data(day=6)
 
 
 def part1():
-    counters = [Counter() for i in range(len('bgpmxqws'))]
-    for i in d.lines():
-        for j, c in enumerate(i):
-            counters[j].update(c)
+    counters = [Counter() for _ in range(len('bgpmxqws'))]
+    for line in d.lines():
+        for ctr, char in zip(counters, line):
+            ctr.update(char)
 
     message = ''.join([c.most_common(1)[0][0] for c in counters])
     print(message)
 
 
 def part2():
-    counters = [Counter() for i in range(len('bgpmxqws'))]
-    for i in d.lines():
-        for j, c in enumerate(i):
-            counters[j].update(c)
+    counters = [Counter() for _ in range(len('bgpmxqws'))]
+    for line in d.lines():
+        for ctr, char in zip(counters, line):
+            ctr.update(char)
 
     message = ''.join([c.most_common()[-1][0] for c in counters])
     print(message)
