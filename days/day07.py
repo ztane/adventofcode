c69cfdc9ce425352f712b546d62d67a55f07f489
Antti Haapala <antti.haapala@proxion.fi>
2016-12-07 07:39:53 +0200

    nicer code with netsplit function

--- days/day07.py
+++ days/day07.py
@@ -4,43 +4,54 @@ from helpers import *
 d = get_aoc_data(day=7)
 
 
-def has_abba(s):
-    for i in range(len(s) - 3):
-        part = s[i:i+4]
+def has_abba(net):
+    """
+    Return true if given net has ABBA
+    :param net: the super/hypernet
+    :return: True if it has ABBA
+    """
+    for i in range(len(net) - 3):
+        part = net[i:i + 4]
         if part[0] == part[3] and part[1] == part[2] and part[0] != part[1]:
             return True
+
+
+def netsplit(s):
+    """
+    Return supers, hypers
+    :param s: the string
+    :return: sequence of (supers, hypers)
+    """
+    nets = re.split('\\[(.*?)]', s)
+    rv = [[], []]
+    for i, net in enumerate(nets):
+        rv[i % 2].append(net)
+
+    return rv
 
 
 def part1():
     cnt = 0
     for i in d.lines():
-        seqs = re.split('\\[(.*?)]', i)
-        has_abba_in_supernet = False
+        supers, hypers = netsplit(i)
 
-        for j, item in enumerate(seqs):
-            # hypernet/supernet
-            if j % 2 and has_abba(item):
-                # has *an* ABBA in hypernet
-                break
+        if not any(map(has_abba, supers)):
+            continue
 
-            else:
-                # or the value for supernets
-                has_abba_in_supernet |= bool(has_abba(item))
-
-        else:
-            cnt += has_abba_in_supernet
+        if not any(map(has_abba, hypers)):
+            cnt += 1
 
     print(cnt)
 
 
-def get_babs(s):
+def get_babs(net):
     """
     For each ABA, yield the corresponding BAB
-    :param s: the string where to find ABAs
+    :param net: the string where to find ABAs
     :return: generator of BABs
     """
-    for i in range(len(s) - 2):
-        part = s[i:i+3]
+    for i in range(len(net) - 2):
+        part = net[i:i + 3]
         if part[0] == part[2] and part[0] != part[1]:
             yield part[1] + part[0] + part[1]
 
@@ -48,22 +59,11 @@ def get_babs(s):
 def part2():
     cnt = 0
     for i in d.lines():
-        seqs = re.split('\\[(.*?)]', i)
-        babs = []
-        hypernets = []
+        supers, hypers = netsplit(i)
 
-        for j, item in enumerate(seqs):
-            # supernet/hypernet?
-            if j % 2:
-                # supernet
-                babs.extend(get_babs(item))
-            else:
-                # hypernet
-                hypernets.append(item)
-
-        for hypernet in hypernets:
-            for bab in babs:
-                if bab in hypernet:
+        for sn in supers:
+            for bab in get_babs(sn):
+                if any(bab in hyper for hyper in hypers):
                     cnt += 1
                     break
 
