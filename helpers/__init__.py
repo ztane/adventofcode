df165be2a276ccec455bddbc291342ded1e2cb1b
Antti Haapala <antti.haapala@proxion.fi>
2016-12-07 09:34:39 +0200

    convert to ints, floats; added docstrings and such.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,10 +1,16 @@
+# noinspection PyUnresolvedReferences
 from math import *
+# noinspection PyUnresolvedReferences
 from itertools import *
+# noinspection PyUnresolvedReferences
 from functools import *
+# noinspection PyUnresolvedReferences
 from collections import *
+# noinspection PyUnresolvedReferences
 from string import *
-import operator as op
+# noinspection PyUnresolvedReferences
 import re
+import operator as op
 from aocd import get_data
 
 
@@ -20,10 +26,23 @@ class Data:
 
 
 def get_aoc_data(day):
+    """
+    Get the wrapped AOC data for a given day
+    :param day: the day
+    :return: the data
+    """
     return Data(get_data(day=day))
 
 
 def clamp(value, min_, max_):
+    """
+    Clamp the value so that it is at least min_ and at most max_
+    :param value: the value
+    :param min_: the minimum
+    :param max_: the maximum
+    :return: min_, max_, or value, iff min_ <= value <= max_
+    """
+
     if value < min_:
         return min_
     if value > max_:
@@ -43,4 +62,32 @@ def ngrams(n, value):
 
 
 def items(thing, *indexes):
+    """
+    Return the given indexes of the item as a tuple
+    :param thing: the thing to index
+    :param indexes: indexes
+    :return: tuple of the items
+    """
     return op.itemgetter(*indexes)(thing)
+
+
+def ints(container):
+    """
+    Return the given items as ints, in the same container type
+    :param container: the items
+    :return: the items as ints
+    """
+
+    t = type(container)
+    return t(map(int, container))
+
+
+def floats(container):
+    """
+    Return the given items as floats, in the same container type
+    :param container: the items
+    :return: the items as ints
+    """
+
+    t = type(container)
+    return t(map(float, container))
