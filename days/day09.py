66e09ee0fc91c2b0a0db55480a4d2e3fc64ff6a4
Antti Haapala <antti.haapala@proxion.fi>
2016-12-09 07:24:47 +0200

    solution to day 9, oh man...

--- days/day09.py
+++ days/day09.py
@@ -2,11 +2,36 @@ from helpers import *
 
 d = get_aoc_data(day=9)
 
+marker_re = re.compile(r'\((\d+)x(\d+)\)')
+
+
+def get_length(s, recursive=False):
+    total = 0
+    pos = 0
+    while pos < len(s.strip()):
+        m = marker_re.match(s, pos)
+        if m:
+            length, repeats = get_ints(m.group(0))
+            pos += len(m.group(0))
+
+            if recursive:
+                total += repeats * get_length(s[pos:pos+length], True)
+            else:
+                total += repeats * length
+
+            pos += length
+
+        else:
+            total += 1
+            pos += 1
+
+    return total
+
 
 def part1():
-    ...
+    print('uncompressed length is', get_length(d.data))
 
 
 def part2():
-    ...
+    print('recursively uncompressed length is', get_length(d.data, True))
 
