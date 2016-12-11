80c33e3db27d9505054230dc0877e1be50adabf4
Antti Haapala <antti.haapala@proxion.fi>
2016-12-11 22:04:19 +0200

    day11 solution

--- days/day11.py
+++ days/day11.py
@@ -1,26 +1,31 @@
-from helpers import *
-import sys
-sys.setrecursionlimit(10000)
+import re
+
+from collections import deque
+from helpers import get_aoc_data, Parser
+
 d = get_aoc_data(day=11)
-print(d.data)
+lineparser = Parser('The <> floor contains <>.')
 
-floor_components = (
-    (3, 1),
-    (0, 2),
-    (2, 2),
-    (0, 0)
-)
+states = []
+for i in d.lines:
+    _, items = lineparser(i)
+    items_split = re.split(', | and ', items)
+    generators, microchips = 0, 0
 
-#floor_components = (
-#    (0, 2),
-#    (1, 0),
-#    (1, 0),
-#    (0, 0)
-#)
+    for j in items_split:
+        if j != 'nothing relevant':
+            if 'generator' in j:
+                generators += 1
+            else:
+                microchips += 1
 
+    states.append((generators, microchips))
 
 
 def fries(comps):
+    """
+    Returns true if something will be fried in the state
+    """
     for g, c in comps:
         if g and g < c:
             return True
@@ -28,82 +33,49 @@ def fries(comps):
     return False
 
 
-minimums = {
-    (((0, 0), (0, 0), (0, 0), (5, 5)), 3): (0, []),
-    (((0, 0), (0, 0), (0, 0), (2, 2)), 3): (0, []),
-    (((0, 0), (0, 0), (0, 0), (7, 7)), 3): (0, [])
-}
+def solve(components):
+    open_nodes = deque()
+    open_nodes.append((components, 0, 0))
 
-currently_visited = set()
+    visited = set()
 
-
-def get_min(comps, floor):
-    try:
-        return minimums[comps, floor]
-    except KeyError:
-        pass
-
-    if fries(comps):
-        return 100000000, []
-
-    minimum = 100000000
-    minimum_tail = []
-    for move in (1, -1):
-        if not (0 <= floor + move <= 3):
-            continue
-
-        if move > 0:
-            order = ((1, 1), (2, 0), (0, 2), (0, 1), (1, 0))
-        else:
-            order = ((0, 1), (1, 0), (1, 1), (2, 0), (0, 2))
-
-        for i, (ge, ce) in enumerate(order):
-            g, c = comps[floor]
-            if g < ge or c < ce:
-                continue
-
-            comps_copy = list(comps)
-            comps_copy[floor] = g - ge, c - ce
+    while True:
+        comps, floor, distance = open_nodes.popleft()
+        for move in (1, -1):
             new_floor = floor + move
-            tg, tc = comps_copy[new_floor]
-            comps_copy[new_floor] = tg + ge, tc + ce
-            floors_t = (tuple(comps_copy), new_floor)
-
-            if floors_t in currently_visited:
+            if not (0 <= new_floor <= 3):
                 continue
 
-            currently_visited.add(floors_t)
-            score, tail = get_min(floors_t[0], new_floor)
-            currently_visited.discard(floors_t)
+            if move > 0:
+                order = ((1, 1), (2, 0), (0, 2), (0, 1), (1, 0))
+            else:
+                order = ((0, 1), (1, 0), (1, 1), (2, 0), (0, 2))
 
-            if score + 1 < minimum:
-                minimum = score + 1
-                minimum_tail = [(ge, ce, move)] + tail
+            for i, (ge, ce) in enumerate(order):
+                g, c = comps[floor]
+                if g < ge or c < ce:
+                    continue
 
-            # can't do better
-            if minimum <= 1:
-                minimums[comps, floor] = minimum, minimum_tail
-                return minimum, minimum_tail
+                comps_copy = list(comps)
+                comps_copy[floor] = g - ge, c - ce
+                tg, tc = comps_copy[new_floor]
+                comps_copy[new_floor] = tg + ge, tc + ce
+                floors_t = (tuple(comps_copy), new_floor)
 
-    minimums[comps, floor] = minimum, minimum_tail
-    return minimum, minimum_tail
-
-
-def solve():
-    print(get_min(floor_components, 0))
-    print(len(minimums))
-
+                if floors_t not in visited and not fries(comps_copy):
+                    visited.add(floors_t)
+                    open_nodes.append(floors_t + (distance + 1,))
+                    if comps[0:3] == ((0, 0), (0, 0), (0, 0)):
+                        print(distance)
+                        return
 
 def part1():
-    solve()
-
+    solve(tuple(states))
 
 def part2():
-    global floor_components
-    floor_components = (
-        (5, 3),
-        (0, 2),
-        (2, 2),
-        (0, 0)
-    )
-    solve()
+    """
+    Add 2 generators, chips
+    """
+
+    states[0] = states[0][0] + 2, states[0][1] + 2
+    solve(tuple(states))
