a5866a3bad67819df3a1d57b7615cd940bd0c815
Antti Haapala <antti.haapala@proxion.fi>
2016-12-14 10:21:54 +0200

    as wim glenn noticed, this is not the matter of "which quintet occurs first" but "which triplet", thus we need to complete the 1000 hashes following the last triplet to see that they don't contain a quintet for a triplet that occurs *earlier* than the current maximum.

--- days/day14.py
+++ days/day14.py
@@ -1,4 +1,8 @@
-from helpers import *
+from hashlib import md5
+from itertools import count
+import re
+
+from helpers import get_aoc_data, defaultdict
 
 start = get_aoc_data(day=14).data
 
@@ -26,25 +30,32 @@ def hashes_part2():
 def find_64th(gen):
     need5 = defaultdict(list)
     hash_count = 0
+    hash_indexes = []
+    upper_limit = 2 ** 31
     for i, current_hash in gen:
+        if i > upper_limit:
+            break
+
         m3 = has_triple.search(current_hash)
         m5 = has_5.search(current_hash)
         if m5:
             triple = m5.group(0)[:3]
-            threekeys = need5[triple]
-            for j in list(threekeys):
+            candidate_indexes = need5.pop(triple)
+            for j in candidate_indexes:
                 if i - j <= 1000:
-                    hash_count += 1
-                    if hash_count == 64:
-                        print('found key #{}'.format(hash_count))
-                        return j
+                    hash_indexes.append(j)
+                    if len(hash_indexes) == 64:
+                        upper_limit = max(hash_indexes) + 1000
+                        print('found key #{}'.format(len(hash_indexes)))
+                        print('hashing 1000 more to ensure completion')
 
-            del threekeys[:]
-            print('found key #{}'.format(hash_count), end='\r')
+            print('found key #{}'.format(len(hash_indexes)), end='\r')
 
         if m3:
             marker = m3.group(0)
             need5[marker].append(i)
+
+    return sorted(hash_indexes)[63]
 
 
 def part1():
