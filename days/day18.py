ce14cdf53906a754a6916ab62eeacabf57b51e55
Antti Haapala <antti.haapala@proxion.fi>
2016-12-18 08:54:39 +0200

    0.3 s with gmpy.popcount

--- days/day18.py
+++ days/day18.py
@@ -1,5 +1,11 @@
 from helpers import get_aoc_data
 
+try:
+    from gmpy import popcount
+except ImportError:
+    def popcount(n):
+        return bin(n).count('1')
+
 d = get_aoc_data(day=18)
 
 
@@ -7,15 +13,16 @@ def solve(data, rows):
     data_len = len(data)
 
     line = int(data.replace('^', '1').replace('.', '0'), 2)
-    safe_count = data.count('.')
 
+    # it is easier to count 1's, so we subtract traps from safe tiles
+    safe_count = data_len * rows - data.count('^')
+
+    # calculate the bitmask for a row
     mask = (1 << data_len) - 1
-    fmt = '0{}b'.format(data_len)
-
     for _ in range(rows - 1):
         # need to mask here, as otherwise we would get more and more bits
         line = ((line << 1) ^ (line >> 1)) & mask
-        safe_count += format(line, fmt).count('0')
+        safe_count -= popcount(line)
 
     return safe_count
 
