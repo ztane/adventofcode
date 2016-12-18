e9e34f80f6eb5f80f1cc8cc508929033c63eae4f
Antti Haapala <antti.haapala@proxion.fi>
2016-12-18 08:29:59 +0200

    day 18 solution

--- days/day18.py
+++ days/day18.py
@@ -1,12 +1,32 @@
-from helpers import *
+from helpers import get_aoc_data
 
 d = get_aoc_data(day=18)
 
 
+def solve(data, rows):
+    data_len = len(data)
+
+    line = int(data.replace('^', '1').replace('.', '0'), 2)
+    untrapped_count = data.count('.')
+
+    mask = (1 << data_len) - 1
+    fmt = '0{}b'.format(data_len)
+
+    for _ in range(rows - 1):
+        # need to mask here, as otherwise we would get more and more bits
+        line = ((line << 1) ^ (line >> 1)) & mask
+        untrapped_count += format(line, fmt).count('0')
+
+    return untrapped_count
+
+
+assert solve('.^^.^.^^^^', rows=10) == 38
+
+
 def part1():
-    ...
+    print('answer', solve(d.data, rows=40))
 
 
 def part2():
-    ...
+    print('answer', solve(d.data, rows=400000))
 
