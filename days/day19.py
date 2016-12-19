8752e64f43523324750b5b012515de53dc7eaa27
Antti Haapala <antti.haapala@proxion.fi>
2016-12-19 08:28:33 +0200

    day 19 solution

--- days/day19.py
+++ days/day19.py
@@ -6,36 +6,59 @@ d = get_aoc_data(day=19)
 
 
 def solve_part1(n_elves):
+    """
+    Solve the part 1 problem for n elves
+    :param n_elves: the number of elves
+    :return: the remaining one
+    """
     elves = deque(range(1, n_elves + 1))
 
+    # piece of cake
     while len(elves) > 1:
-        e = elves.popleft()
+        # move one on the other side
+        elves.append(elves.popleft())
+        # and eliminate the next one
         elves.popleft()
-        elves.append(e)
 
     return elves[0]
 
 
 def solve_part2(n_elves):
-    elves = list(range(1, n_elves + 1))
-    elves_remaining = n_elves
+    """
+    Solve the part 2 problem for n elves
+    :param n_elves: the number of elves
+    :return: the remaining one
+    """
+
+    elves = range(1, n_elves + 1)
     on_left = deque()
     on_right = deque()
+
+    # I am **ignoring** the left/right distinction on the deque. I really do not
+    # think deque as left-right; instead I always think append/pop operating on
+    # the end of it, and `*left` doing the opposite.
+    #
+    # Thus instead instead the "left" in either queue means now **closest** to
+    # the current elf; and the "right" is furthest away - that is, deque indices
+    # grow as we move further from the current elf.
     on_left.extend(elves[:len(elves) // 2])
-    on_right.extend(elves[len(elves) // 2:][::-1])
+    on_right.extendleft(elves[len(elves) // 2:])
 
     while True:
+        # sometimes the left queue is empty and both are on the right side;
+        # then pull the elf on the furthest of right side and pop it to the left
         if not on_left:
             on_left.append(on_right.pop())
 
+        # remove the current elf from queues. Always the next one from left
         current_elf = on_left.popleft()
-        elves_remaining -= 1
+        n_elves -= 1
 
-        # balance the sides
-        if elves_remaining % 2:
-            tgt = elves_remaining // 2 + 1
+        # balance the sides so that left side has one more elf
+        if n_elves % 2:
+            tgt = n_elves // 2 + 1
         else:
-            tgt = elves_remaining
+            tgt = n_elves
 
         while len(on_left) > tgt:
             on_right.append(on_left.pop())
@@ -46,10 +69,11 @@ def solve_part2(n_elves):
         # eliminate the elf on the left or just opposite
         on_left.pop()
 
-        # and replace the current_elf to the right side of the next one
-        on_right.appendleft(current_elf)
-        if elves_remaining == 1:
+        if n_elves == 1:
             return current_elf
+
+        # otherwise move the current_elf to the right side of the next one
+        on_right.appendleft(current_elf)
 
 
 assert solve_part1(5) == 3
